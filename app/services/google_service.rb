7f50df5d491ad3ac5689fb1c9bcebe0ae20f9ebd
Trevor Nodland <trevor.nodland@gmail.com>
2019-06-03 10:19:25 -0600

    adds get_city method to googleserivce

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,10 +1,5 @@
 class GoogleService
   def get_coords(location)
-    conn = Faraday.new("https://maps.googleapis.com/maps/api/") do |f|
-      f.adapter Faraday.default_adapter
-      f.params[:key] = ENV['GOOGLE-API-KEY']
-    end
-
     response = conn.get("geocode/json") do |req|
       req.params[:address] = location
     end
@@ -12,4 +7,21 @@ class GoogleService
     json = JSON.parse(response.body, symbolize_names: true)
     return json[:results][0][:geometry][:location]
   end
+
+  def get_city(lat, long)
+    response = conn.get("geocode/json") do |req|
+      req.params[:latlng] = "#{lat},#{long}"
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    return json[:results][0][:address_components][1][:long_name]
+  end
+
+  private
+    def conn
+      Faraday.new("https://maps.googleapis.com/maps/api/") do |f|
+        f.adapter Faraday.default_adapter
+        f.params[:key] = ENV['GOOGLE-API-KEY']
+      end
+    end
 end
